81a11ebd134dd31f620268a1349a9e2e80950df3
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-09-12 11:38:07 -0500

    Cambio a .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-// App.jsx or index.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// App.jsx
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import MessagePage from './components/MessagePage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Ruta por defecto que redirige a /message/1 */}
+        <Route path="/" element={<Navigate to="/message/1" replace />} />
+        
+        {/* Ruta que muestra el mensaje basado en el ID */}
         <Route path="/message/:id" element={<MessagePage />} />
       </Routes>
     </Router>
